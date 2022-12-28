23c68c0b230d5c8c4ff3c65048bcd6e99bd9bccb
Tarequl Islam <tarequl.i@coppanet.com>
2022-12-28 16:30:40 +0600

    revision of R-1500

---
+++ Codeforces/1163 b1.Cat party.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define CLR(a, b) memset(a, b, sizeof(a))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626433832795028841971
+#define M 998244353
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+const int MOD = 1000 * 1000 * 1000 + 7;
+const int MAXN = 10 * 1000 * 1000 + 10, MAXV = 4;
+ll GCD(ll a, ll b) { return (a % b) == 0 ? b : GCD(b, a % b); }
+ll mod(ll x) { return ((x % M + M) % M); }
+bool cmp(const pii &left, const pii &right){
+    return left.first > right.first || (left.first == right.first && left.second < right.second); }
+//int find(int x) { return (p[x] == x ? x : p[x] = find(p[x])); } //p[find(i)]=find(j);
+int visited[10];
+vector<int>vec[10];
+void dfs(int at){
+    visited[at] = 1;
+    cout<<at<<" ";
+    for (int i = 0; i < vec[at].size(); i++){
+        if(visited[vec[at][i]]==0)
+            dfs(vec[at][i]);
+    }
+}
+
+
+const int N = 1e5 + 10;
+
+int n, color, ans, mx, f[N], cnt[N];
+
+int main(){
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++){
+        scanf("%d", &color);
+        cnt[f[color]]--;
+        f[color]++;
+        cnt[f[color]]++;
+        mx = max(mx, f[color]);
+        bool ok = false;
+        if (cnt[1] == i) // every color has occurence of 1
+            ok = true;
+        else if (cnt[i] == 1) // only one color has the maximum occurence and the occurence is i
+            ok = true;
+        else if (cnt[1] == 1 && cnt[mx] * mx == i - 1) // one color has occurence of 1 and other colors have the same occurence
+            ok = true;
+        else if (cnt[mx - 1] * (mx - 1) == i - mx && cnt[mx] == 1) // one color has the occurence 1 more than any other color
+            ok = true;
+        if (ok)
+            ans = i;
+    }
+    printf("%d", ans);
+	return 0;
+}
